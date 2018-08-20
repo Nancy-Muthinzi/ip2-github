55241c91665848cf6c6b55218f03b0c70ec04be5
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-08-20 14:05:23 +0300

    add class repos for my repositories

--- src/app/github-http/request.service.ts
+++ src/app/github-http/request.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { environment } from '../../environments/environment'
 import { Nancy } from '../nancy'
+import { Repos } from '../repos'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
   homepage: Nancy;
+
 
   constructor(private http: HttpClient) {
     this.homepage = new Nancy("", "", "")
@@ -17,25 +19,42 @@ export class RequestService {
     interface ApiResponse {
       avatar_url: any;
       login: string;
-      repos: string;
     }
 
     let promise = new Promise((resolve, reject) => {
       this.http.get<ApiResponse>("https://api.github.com/users/Nancy-Muthinzi?access_token=" + environment.apiUrl).toPromise().then(response => {
-        this.homepage.avatar_url= response.avatar_url
-        this.homepage.login= response.login
-        this.homepage.repos=response.repos
+        this.homepage.avatar_url = response.avatar_url
+        this.homepage.login = response.login
         console.log(this.homepage.repos)
         resolve()
       },
-      error=>{
-        this.homepage.avatar_url="not found"
-        this.homepage.login="not found"
-        this.homepage.repos="not found"
-        reject(error)
-      }
+        error => {
+          this.homepage.avatar_url = "not found"
+          this.homepage.login = "not found"
+          this.homepage.repos = "not found"
+          reject(error)
+        }
       )
     })
     return Promise
+
+    reposRequest() {
+      interface ApiResponse {
+        repos: any;
+      }
+      let promise = new Promise((resolve, reject) => {
+        this.http.get<ApiResponse>("https://api.github.com/users/Nancy-Muthinzi/repos?access_token=" + environment.apiUrl).toPromise().then(response => {
+          this.homepage.repos = response.repos
+          console.log(this.homepage.repos)
+          resolve()
+        },
+          error => {
+            this.homepage.repos = "not found"
+            reject(error)
+          }
+        )
+      })
+      return Promise
+    }
   }
 }
